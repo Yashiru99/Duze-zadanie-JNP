93b688bfa8bdb9d18f906a43b813f272ff4f0e60
Julian Kozłowski <jk417694@students.mimuw.edu.pl>
2021-05-02 10:59:22 +0200

    ladna wersja bez komentarzy

--- src/poly.c
+++ src/poly.c
@@ -9,10 +9,8 @@
       exit(1);        \
     }                 \
   } while (0)
-#define C PolyFromCoeff
 
-
-Mono *MergeTwoMonoArrays(size_t sizeP, size_t sizeQ, const Mono *p, const Mono *q) {
+static Mono *MergeTwoMonoArrays(size_t sizeP, size_t sizeQ, const Mono *p, const Mono *q) {
     // tworzymy głęboką kopię skopiowanej tablicy
     Mono *mergedArray = malloc(sizeof(Mono) * (sizeP + sizeQ));
     CHECK_PTR(mergedArray);
@@ -32,7 +30,7 @@ Poly PolyAdd(const Poly *p, const Poly *q) {
 
     Poly result;
     Mono coeffPoly;
-    Mono *mergedArray; // przygotowanie głębokiej kopii
+    Mono *mergedArray;
 
 
     if (qIsCoeff && pIsCoeff) {
@@ -57,18 +55,15 @@ Poly PolyAdd(const Poly *p, const Poly *q) {
 
 
 void PolyDestroy(Poly *p) {
-    if (PolyIsCoeff(p)) {
-        return;
-    }
-    else{
+    if (!PolyIsCoeff(p)) {
         for(size_t j = 0; j < p -> size; ++j){
             PolyDestroy(&p -> arr[j].p);
         }
+        free(p -> arr);
     }
-    free(p -> arr);
 }
 
-int MonoComparator(const void *a, const void *b) {
+static int MonoComparator(const void *a, const void *b) {
     Mono *_a = (Mono *) a;
     Mono *_b = (Mono *) b;
     if (_a->exp < _b->exp) return -1;
@@ -76,35 +71,34 @@ int MonoComparator(const void *a, const void *b) {
     else return 1;
 }
 
-void SortMonoArray(Mono *arr, size_t count){
+static void SortMonoArray(Mono *arr, size_t count){
     qsort(arr, count, sizeof(Mono), MonoComparator);
 }
-bool CanAddTwoMonos(const Mono a, const Mono b) {
+
+static bool CanAddTwoMonos(const Mono a, const Mono b) {
     return a.exp == b.exp;
 }
 
 
-void shiftArray(size_t *size, Mono *array, size_t k){
-    for(size_t i = k; i < *size - 1; i++){
+static void ShiftArray(size_t *size, Mono *array, size_t index){
+    for(size_t i = index; i < *size - 1; i++){
         array[i] = array[i+1];
     }
     (*size)--;
 }
 
-Mono *simplify(Mono *arr, const size_t count, size_t *realCount) {
-
-
+static Mono *Simplify(Mono *arr, const size_t count, size_t *realCount) {
 
     *realCount = count;
     // finalnie nic nie zmienamy w arr
     // upraszczanie przez dodawanie
-    for(size_t j = 0; j < *realCount - 1; ++j){
-        if(CanAddTwoMonos(arr[j], arr[j+1])){
-            Mono toBeDestroyed = arr[j];
-            arr[j].p = PolyAdd(&arr[j].p, &arr[j + 1].p);
+    for(size_t j = 1; j < *realCount; ++j){
+        if(CanAddTwoMonos(arr[j - 1], arr[j])){
+            Mono toBeDestroyed = arr[j - 1];
+            arr[j - 1].p = PolyAdd(&arr[j - 1].p, &arr[j].p);
             MonoDestroy(&toBeDestroyed);
-            MonoDestroy(&arr[j + 1]);
-            shiftArray(realCount, arr, j + 1);
+            MonoDestroy(&arr[j]);
+            ShiftArray(realCount, arr, j);
             j--;
         }
     }
@@ -113,7 +107,7 @@ Mono *simplify(Mono *arr, const size_t count, size_t *realCount) {
     while(k < *realCount){
         if(PolyIsZero(&arr[k].p)){
             MonoDestroy(&arr[k]);
-            shiftArray(realCount, arr, k);
+            ShiftArray(realCount, arr, k);
         }
         else{
             k++;
@@ -128,13 +122,13 @@ Mono *simplify(Mono *arr, const size_t count, size_t *realCount) {
 Poly PolyAddMonos(size_t count, const Mono monos[]) {
     if(count == 0) return (Poly) {.coeff = 0, .arr = NULL};
 
-    Mono *CopyOfMonos = calloc(count, sizeof(Mono));
+    Mono *CopyOfMonos = malloc(count * sizeof(Mono));
     CHECK_PTR(CopyOfMonos);
 
     memcpy(CopyOfMonos, monos, count * sizeof(Mono));
     SortMonoArray(CopyOfMonos, count);
     size_t realCount = 0;
-    simplify(CopyOfMonos, count, &realCount);
+    Simplify(CopyOfMonos, count, &realCount);
 
     if(realCount == 0) {
         free(CopyOfMonos);
@@ -147,20 +141,20 @@ Poly PolyClone(const Poly *p) {
     if (PolyIsCoeff(p)) {
         return PolyFromCoeff(p -> coeff);
     }
-    Mono *new_array = malloc(sizeof(Mono) * p->size);
+    Mono *newArray = malloc(sizeof(Mono) * p->size);
     for(size_t i = 0; i < p -> size; ++i) {
-        Poly new_poly = PolyClone(&p->arr[i].p);
-        new_array[i] = MonoFromPoly(&new_poly, p->arr[i].exp);
+        Poly newPoly = PolyClone(&p->arr[i].p);
+        newArray[i] = MonoFromPoly(&newPoly, p->arr[i].exp);
     }
 
-    return (Poly) {.size = p -> size, .arr = new_array};
+    return (Poly) {.size = p -> size, .arr = newArray};
 }
 
-bool MonoIsEq(const Mono *p, const Mono *q) {
+static bool MonoIsEq(const Mono *p, const Mono *q) {
     return (p->exp == q->exp) && PolyIsEq(&p->p, &q->p);
 }
 
-bool WholePolyIsCoeff(const Poly *p) {
+static bool WholePolyIsCoeff(const Poly *p) {
     if (PolyIsCoeff(p))return true;
     if (p->size == 1 && p->arr[0].exp == 0 && WholePolyIsCoeff(&p->arr[0].p)) {
         return true;
@@ -168,12 +162,9 @@ bool WholePolyIsCoeff(const Poly *p) {
     return false;
 }
 
-Poly returnRealCoeff(const Poly *p) {
+static Poly returnRealCoeff(const Poly *p) {
     if (!PolyIsCoeff(p))return returnRealCoeff(&p->arr[0].p);
-    Poly result;
-    result.arr = NULL;
-    result.coeff = p->coeff;
-    return result;
+    return (Poly) {.arr = NULL, .coeff = p -> coeff};
 }
 
 bool PolyIsEq(const Poly *p, const Poly *q) {
@@ -183,7 +174,8 @@ bool PolyIsEq(const Poly *p, const Poly *q) {
     if (WholePolyIsCoeff(p) || WholePolyIsCoeff(q)) {
         return false;
     }
-    if (!WholePolyIsCoeff(p) && !WholePolyIsCoeff(q) && p->size == q->size) {
+    if(p -> size != q -> size) return false;
+    else{
         for (size_t i = 0; i < p->size; i++) {
             if (!MonoIsEq(&p->arr[i], &q->arr[i])) {
                 return false;
@@ -200,7 +192,7 @@ Poly PolyNeg(const Poly *p) {
     return result;
 }
 
-poly_exp_t max(poly_exp_t a, poly_exp_t b) {
+static poly_exp_t Max(poly_exp_t a, poly_exp_t b) {
     return a > b ? a : b;
 }
 
@@ -215,32 +207,31 @@ Poly PolySub(const Poly *p, const Poly *q) {
 poly_exp_t PolyDeg(const Poly *p) {
     poly_exp_t result = -1;
     if (PolyIsZero(p)) return result;
-    if (PolyIsCoeff(p))return 0;
+    if (PolyIsCoeff(p)) return 0;
     if (p->size == 1) {
         result = p->arr[0].exp;
         return result;
     } else {
         for (size_t i = 0; i < p->size; i++) {
-            result = max(result, p->arr[i].exp * PolyDeg(&p->arr[i].p));
+            result = Max(result, p->arr[i].exp * PolyDeg(&p->arr[i].p));
         }
     }
     return result;
 }
 
-poly_exp_t PolyDegBy(const Poly *p, size_t var_idx) {
+poly_exp_t PolyDegBy(const Poly *p, size_t varIdX) {
     poly_exp_t result = -1;
     if (PolyIsZero(p))return -1;
     if (WholePolyIsCoeff(p))return 0;
-    if (var_idx == 0) {
+    if (varIdX == 0) {
         return p->arr[p->size - 1].exp;
     } else {
         for (size_t i = 0; i < p->size; ++i) {
-            result = max(PolyDegBy(&p->arr[i].p, var_idx - 1), result);
+            result = Max(PolyDegBy(&p->arr[i].p, varIdX - 1), result);
         }
     }
     return result;
 }
-
 
 void MultiplyByScalar(Poly *p, poly_coeff_t x) {
     if (PolyIsCoeff(p)) {
@@ -252,72 +243,75 @@ void MultiplyByScalar(Poly *p, poly_coeff_t x) {
     }
 }
 
-Poly PolyMul(const Poly *p, const Poly *q) {
-    bool p_is_coeff = PolyIsCoeff(p);
-    bool q_is_coeff = PolyIsCoeff(q);
-
-    if (p_is_coeff && q_is_coeff) {
-        return PolyFromCoeff(p->coeff * q->coeff);
-    }
-
-    if (p_is_coeff) {
-        return PolyMul(q, p);
-    }
-
-    if (q_is_coeff) {
-        // jeśli mnożymy wielomian przez stałą, to mnożymy współczynniki przy wszystkich monomianach
-
-        Mono* new_array = (Mono*) calloc(p->size, sizeof(Mono));
+static Poly MultiplyPolyByScalar(const Poly *p, const Poly *q){
+    Mono* newArray = (Mono*) malloc(p->size * sizeof(Mono));
 
         for (size_t i = 0; i < p->size; i++) {
-            Poly tmp_poly = PolyMul(&(p->arr[i].p), q);
+            Poly tempPoly = PolyMul(&(p->arr[i].p), q);
 
-            if (PolyIsZero(&tmp_poly)) {
-                new_array[i] = MonoFromPoly(&tmp_poly, 0);
+            if (PolyIsZero(&tempPoly)) {
+                newArray[i] = MonoFromPoly(&tempPoly, 0);
             } else {
-                new_array[i] = MonoFromPoly(&tmp_poly, p->arr[i].exp);
+                newArray[i] = MonoFromPoly(&tempPoly, p->arr[i].exp);
             }
         }
 
-        Poly res = PolyAddMonos(p->size, new_array);
-        free(new_array);
-        return res;
-    }
+    Poly res = PolyAddMonos(p->size, newArray);
+    free(newArray);
+    return res;
+}
 
-    // ostateczny przypadek: mnożymy przez siebie dwa wielomiany stopnia > 0
-
-    Poly result_poly = PolyZero();
+static Poly MultiplyPolyByPoly(const Poly *p, const Poly *q){
+    Poly resultPoly = PolyZero();
 
     for (size_t i = 0; i < p->size; i++) {
         Mono *new_array = (Mono*) malloc(q->size * sizeof(Mono));
 
         for (size_t j = 0; j < q->size; j++) {
-            Poly new_poly = PolyMul(&(p->arr[i].p), &(q->arr[j].p));
-            poly_exp_t new_exp = MonoGetExp(&(p->arr[i])) + MonoGetExp(&(q->arr[j]));
+            Poly newPoly = PolyMul(&(p->arr[i].p), &(q->arr[j].p));
+            poly_exp_t newExp = MonoGetExp(&(p->arr[i])) + MonoGetExp(&(q->arr[j]));
 
-            if (PolyIsZero(&new_poly)) {
-                new_exp = 0;
+            if (PolyIsZero(&newPoly)) {
+                newExp = 0;
             }
 
-            new_array[j] = MonoFromPoly(&new_poly, new_exp);
+            new_array[j] = MonoFromPoly(&newPoly, newExp);
         }
 
-        Poly add_to_result_poly = PolyAddMonos(q->size, new_array);
-        Poly tmp_res_poly = PolyAdd(&result_poly, &add_to_result_poly);
+        Poly addToResult = PolyAddMonos(q->size, new_array);
+        Poly finalPoly = PolyAdd(&resultPoly, &addToResult);
 
         free(new_array);
-        PolyDestroy(&result_poly);
-        PolyDestroy(&add_to_result_poly);
+        PolyDestroy(&resultPoly);
+        PolyDestroy(&addToResult);
 
-        result_poly = tmp_res_poly;
+        resultPoly = finalPoly;
     }
-
-    return result_poly;
+    return resultPoly;
 }
 
-poly_coeff_t power(poly_coeff_t coefficient, poly_exp_t exp) {
+Poly PolyMul(const Poly *p, const Poly *q) {
+    bool pIsCoeff = PolyIsCoeff(p);
+    bool qIsCoeff = PolyIsCoeff(q);
+
+    if (pIsCoeff && qIsCoeff) {
+        return PolyFromCoeff(p->coeff * q->coeff);
+    }
+
+    if (pIsCoeff) {
+        return PolyMul(q, p);
+    }
+
+    if (qIsCoeff) {
+        return MultiplyPolyByScalar(p, q);
+    }
+
+    return MultiplyPolyByPoly(p, q);
+}
+
+static poly_coeff_t Power(poly_coeff_t coefficient, poly_exp_t exp) {
     if(exp == 0) return 1;
-    poly_coeff_t temporary = power(coefficient, exp / 2);
+    poly_coeff_t temporary = Power(coefficient, exp / 2);
     if(exp % 2 == 0){
         return temporary * temporary;
     }
@@ -327,21 +321,20 @@ poly_coeff_t power(poly_coeff_t coefficient, poly_exp_t exp) {
 }
 
 Poly PolyAt(const Poly *p, poly_coeff_t x) {
-    Poly result = C(0);
-    Poly *PolyToAdd = malloc(sizeof(Poly) * p -> size);
+    Poly result = PolyZero();
     if (PolyIsCoeff(p)){
-        free(PolyToAdd);
         return *p;
     }
+    Poly *polyToAdd = malloc(sizeof(Poly) * p -> size);
     for (size_t i = 0; i < p->size; i++) {
         if (p->arr[i].exp != 0) {
-            MultiplyByScalar(&p->arr[i].p, power(x, p->arr[i].exp));
+            MultiplyByScalar(&p->arr[i].p, Power(x, p->arr[i].exp));
         }
         result = PolyAdd(&result, &p->arr[i].p);
-        PolyToAdd[i] = result;
+        polyToAdd[i] = result;
     }
-    for(size_t i = 0; i < p -> size - 1; ++i) PolyDestroy(&PolyToAdd[i]);
-    free(PolyToAdd);
+    for(size_t i = 0; i < p -> size - 1; ++i) PolyDestroy(&polyToAdd[i]);
+    free(polyToAdd);
     return result;
 
 }
