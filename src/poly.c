79a412c3ab6c377e87d6671ec0f011fef5634daf
Julian Kozłowski <jk417694@students.mimuw.edu.pl>
2021-05-02 14:28:33 +0200

    dodanie komentarzy doxygenowych

--- src/poly.c
+++ src/poly.c
@@ -1,3 +1,12 @@
+/** @file
+  Interfejs pomocniczych funkcji wykorzystywanych przy implementacji funkcji na wielomianach rzadkich.
+
+  @authors Julian Kozłowski <jk417694@students.mimuw.edu.pl>
+  @copyright Uniwersytet Warszawski
+  @date 2021
+*/
+
+
 #include "poly.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,8 +19,15 @@
     }                 \
   } while (0)
 
+/**
+ * Tworzy nową tablicę jednomianow poprzez sklejenie tablicy p z tablicą q.
+ * @param[in] sizeP : wielkość tablicy monomianow p
+ * @param[in] sizeQ : wielkość tablicy monomianow q
+ * @param[in] p : tablice monomianow
+ * @param[in] q : tablica monomianow
+ * @return tablica monomianow.
+ */
 static Mono *MergeTwoMonoArrays(size_t sizeP, size_t sizeQ, const Mono *p, const Mono *q) {
-    // tworzymy głęboką kopię skopiowanej tablicy
     Mono *mergedArray = malloc(sizeof(Mono) * (sizeP + sizeQ));
     CHECK_PTR(mergedArray);
 
@@ -23,6 +39,7 @@ static Mono *MergeTwoMonoArrays(size_t sizeP, size_t sizeQ, const Mono *p, const
     }
     return mergedArray;
 }
+
 Poly PolyAdd(const Poly *p, const Poly *q) {
     bool qIsCoeff = PolyIsCoeff(q);
     bool pIsCoeff = PolyIsCoeff(p);
@@ -70,16 +87,32 @@ static int MonoComparator(const void *a, const void *b) {
     else if (_a->exp == _b->exp) return 0;
     else return 1;
 }
-
+/**
+ * Sortuje tablicę jednomianow.
+ * @param[in] arr : tablica jednomianow
+ * @param[in] count : dlugosc tablicy arr
+ * @return posortowana tablica monomianow
+ */
 static void SortMonoArray(Mono *arr, size_t count){
     qsort(arr, count, sizeof(Mono), MonoComparator);
 }
 
+/**
+ * Sprawdza czy 2 monomiany mozemy do siebie dodac.
+ * @param[in] a : monomian
+ * @param[in] b : monomian
+ * @return wartość boolean.
+ */
 static bool CanAddTwoMonos(const Mono a, const Mono b) {
     return a.exp == b.exp;
 }
 
-
+/**
+ * Modyfikuje tablicę wielomianow w miejscu oraz jej wielkość, przesuwając wszystkie elementy począwszy od indexu w lewo oraz odpowiednio zmniejszając wielkość o jeden.
+ * @param[in] size : wskaźnik na wielkość
+ * @param[in] array : tablica monomianow
+ * @param[in] index : indeks
+ */
 static void ShiftArray(size_t *size, Mono *array, size_t index){
     for(size_t i = index; i < *size - 1; i++){
         array[i] = array[i+1];
@@ -87,11 +120,16 @@ static void ShiftArray(size_t *size, Mono *array, size_t index){
     (*size)--;
 }
 
+/**
+ * Funkcja upraszczająca tablicę monomianow w miejscu.
+ * @param[in] arr : tablica monomianow
+ * @param[in] count : wielkosc tablicy monomianow
+ * @param[in] realCount : wskaźnik ktory posluzy do zapisu nowej wielkosci
+ * return tablica monomianow
+ */
 static Mono *Simplify(Mono *arr, const size_t count, size_t *realCount) {
 
     *realCount = count;
-    // finalnie nic nie zmienamy w arr
-    // upraszczanie przez dodawanie
     for(size_t j = 1; j < *realCount; ++j){
         if(CanAddTwoMonos(arr[j - 1], arr[j])){
             Mono toBeDestroyed = arr[j - 1];
@@ -118,7 +156,7 @@ static Mono *Simplify(Mono *arr, const size_t count, size_t *realCount) {
     }
     return arr;
 }
-//robimy płytka kopie naszego monos i go zwracamy
+
 Poly PolyAddMonos(size_t count, const Mono monos[]) {
     if(count == 0) return (Poly) {.coeff = 0, .arr = NULL};
 
@@ -151,11 +189,20 @@ Poly PolyClone(const Poly *p) {
 
     return (Poly) {.size = p -> size, .arr = newArray};
 }
-
+/**
+ * Sprawdzanie czy 2 monomianowy są tozsamosciowo sobie rowne
+ * @param[in] p : monomian
+ * @param[in] q : monomian
+ * return boolean
+ */
 static bool MonoIsEq(const Mono *p, const Mono *q) {
     return (p->exp == q->exp) && PolyIsEq(&p->p, &q->p);
 }
-
+/**
+ * Sprawdzanie czy wielomian jest tozsamościowy z wielomianem stałym
+ * @param[in] p : wielomian
+ * return boolean
+ */
 static bool WholePolyIsCoeff(const Poly *p) {
     if (PolyIsCoeff(p))return true;
     if (p->size == 1 && p->arr[0].exp == 0 && WholePolyIsCoeff(&p->arr[0].p)) {
@@ -163,7 +210,11 @@ static bool WholePolyIsCoeff(const Poly *p) {
     }
     return false;
 }
-
+/**
+ * Znajduje wielomian stały tozsamosciowy z wielomianem wejściowym
+ * @param[in] p : wielomian
+ * return wielomian
+ */
 static Poly returnRealCoeff(const Poly *p) {
     if (!PolyIsCoeff(p))return returnRealCoeff(&p->arr[0].p);
     return (Poly) {.arr = NULL, .coeff = p -> coeff};
@@ -193,7 +244,12 @@ Poly PolyNeg(const Poly *p) {
     MultiplyByScalar(&result, -1);
     return result;
 }
-
+/**
+ * Porownywanie 2 wykładnikow
+ * @param[in] a : wykladnik
+ * @param[in] b : wykladnik
+ * return wykladnik
+ */
 static poly_exp_t Max(poly_exp_t a, poly_exp_t b) {
     return a > b ? a : b;
 }
@@ -234,7 +290,12 @@ poly_exp_t PolyDegBy(const Poly *p, size_t varIdX) {
     return result;
 }
 
-void MultiplyByScalar(Poly *p, poly_coeff_t x) {
+/**
+ * Mnozenie wielomianu przez skalar
+ * @param[in] p : wielomian
+ * @param[in] x : skalar
+ */
+static void MultiplyByScalar(Poly *p, poly_coeff_t x) {
     if (PolyIsCoeff(p)) {
         p->coeff *= x;
     } else {
@@ -243,7 +304,12 @@ void MultiplyByScalar(Poly *p, poly_coeff_t x) {
         }
     }
 }
-
+/**
+ * Mnozenie wielomianu przez wielomian ktory jest tozsamościowo stały
+ * @param[in] p : wielomian
+ * @param[in] q : wielomian
+ * return wielomian
+ */
 static Poly MultiplyPolyByScalar(const Poly *p, const Poly *q){
     Mono* newArray = (Mono*) malloc(p->size * sizeof(Mono));
     CHECK_PTR(newArray);
@@ -262,7 +328,12 @@ static Poly MultiplyPolyByScalar(const Poly *p, const Poly *q){
     free(newArray);
     return res;
 }
-
+/**
+ * Mnozenie wielomianu przez wielomian, z ktorych zaden nie jest tozsamościowo stały
+ * @param[in] p : wielomian
+ * @param[in] q : wielomian
+ * return wielomian
+ */
 static Poly MultiplyPolyByPoly(const Poly *p, const Poly *q){
     Poly resultPoly = PolyZero();
 
@@ -311,7 +382,12 @@ Poly PolyMul(const Poly *p, const Poly *q) {
 
     return MultiplyPolyByPoly(p, q);
 }
-
+/**
+ * Funkcja potęgowa działająca w czasie O(log(exp)))
+ * @param[in] coefficient : wspolczynnik
+ * @param[in] exp : wykladnik
+ * return wspolczynnik
+ */
 static poly_coeff_t Power(poly_coeff_t coefficient, poly_exp_t exp) {
     if(exp == 0) return 1;
     poly_coeff_t temporary = Power(coefficient, exp / 2);
