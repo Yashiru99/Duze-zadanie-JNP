709f4ec30341610eaa6da5f1bcc466c6f7469556
Julian Kozłowski <jk417694@students.mimuw.edu.pl>
2021-05-24 23:06:16 +0200

    ostateczna wersja z naprawionym doxygenem v2

--- src/calc.c
+++ src/calc.c
@@ -1,3 +1,10 @@
+/** @file
+  Plik z funkcją main wykonujący głowne zadanie.
+
+  @author Julian Kozłowski <jk417694@students.mimuw.edu.pl>
+  @copyright Uniwersytet Warszawski
+  @date 2021
+*/
 #include "inputPoly.h"
 
 int main(){
